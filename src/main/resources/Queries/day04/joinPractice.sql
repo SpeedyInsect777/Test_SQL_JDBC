20bee8c04e1677ad582e403d10c7a8f5e3ec9eed
SpeedyBEAR777 <60725603+Manu777TJ@users.noreply.github.com>
2022-12-31 21:43:35 -0600

    last update

--- src/main/resources/Queries/day04/joinPractice.sql
+++ src/main/resources/Queries/day04/joinPractice.sql
@@ -1,5 +1,14 @@
 select *from employees;
 /*
-1.Display All First names and Department names
-/
+--1. Display all first_name and department_name
+--2.Display all first_name and department_name including the  department without employee
+--3.Display all first_name and department_name including the  employee without department
+--4.Display all first_name and department_name
+--including the  department without employee  and  employees  without departments
+--5.Display All cities and related country names
+--6.Display All cities and related country names  including with countries without city
+--7.Display all department name and  street adresss
+--8.Display first_name,last_name and department_name,city for all employees
+--9.Display first_name,last_name and department_name,city,country_name for all employees
+
 */
