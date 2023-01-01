9cc36841271597962cd32b5651e08d76d67d443d
SpeedyBEAR777 <60725603+Manu777TJ@users.noreply.github.com>
2022-12-31 21:40:45 -0600

    last update

--- src/main/resources/Queries/day03/rownum.sql
+++ src/main/resources/Queries/day03/rownum.sql
@@ -1,5 +1,26 @@
-select *from Employees;
+select * from employees;
+
+select * from EMPLOYEES where ROWNUM <6 order by SALARY desc ;
+
+
+select * from (select *from EMPLOYEES order by SALARY desc )
+where ROWNUM<6;
+
+--display all info who is getting 5th highest salary
+select min(salary) from (select distinct SALARY from EMPLOYEES order by  SALARY desc )
+        where rownum<6;
 
 select *from EMPLOYEES
-where SALARY < (select min(SALARY) from EMPLOYEES)
-where rownum=5;
+where SALARY = (select min(salary) from (select distinct SALARY from EMPLOYEES order by  SALARY desc )
+                where rownum<6);
+
+
+--display all info who is getting 50th highest salary
+select *from EMPLOYEES
+where SALARY=(select min(SALARY)
+from (select distinct SALARY from EMPLOYEES order by SALARY desc )
+where rownum<51);
+
+select distinct count(*) from EMPLOYEES;
+
+--display all info who is getting 3th lowest salary
