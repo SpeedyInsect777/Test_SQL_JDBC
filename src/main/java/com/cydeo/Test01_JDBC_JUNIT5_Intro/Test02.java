9c1c9a16d5f6a27dcc5df4725326aa3601b71729
SpeedyBEAR777 <60725603+Manu777TJ@users.noreply.github.com>
2023-01-02 02:49:21 -0600

    last update

--- src/main/java/com/cydeo/Test01_JDBC_JUNIT5_Intro/Test02.java
+++ src/main/java/com/cydeo/Test01_JDBC_JUNIT5_Intro/Test02.java
@@ -3,11 +3,17 @@ package com.cydeo.Test01_JDBC_JUNIT5_Intro;
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class Test01 {
+public class Test02 {
+
     String dbURL = "jdbc:oracle:thin:@3.82.233.195 :1521:XE";
     String dbUserName = "hr";
     String dbPassword = "hr";
+
 
     @Test
     public void test01() throws SQLException {
@@ -15,19 +21,23 @@ public class Test01 {
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
                 ResultSet.CONCUR_READ_ONLY);
         ResultSet resultSet = statement.executeQuery
-                ("select *from regions");//CTRL+ENTER to connect to DATABASE
+                ("select first_name,Last_name,salary from employees");//CTRL+ENTER to connect to DATABASE
         ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
 
 
-        int numColumns = resultSetMetaData.getColumnCount();
+        List<Map<String, Object>> mapList = new ArrayList<>();
+
         while (resultSet.next()) {
-
-            for (int i = 1; i < numColumns + 1; i++) {
-
-                System.out.println(resultSet.getString(i) + " | " + resultSetMetaData.getColumnName(i));
+            Map<String, Object> myRowMap = new HashMap<>();
+            for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
+                myRowMap.put( resultSetMetaData.getColumnName(i),resultSet.getString(i));
 
             }
+            mapList.add(myRowMap);
         }
+
+
+        System.out.println(mapList);
 
         resultSet.close();
         statement.close();
---
+++ src/main/java/com/cydeo/utilities/DatabaseCredentials.java
@@ -0,0 +1,12 @@
+package com.cydeo.utilities;
+
+public class DatabaseCredentials {
+
+
+
+
+        String dbURL = "jdbc:oracle:thin:@3.82.233.195 :1521:XE";
+        String dbUserName = "hr";
+        String dbPassword = "hr";
+
+}
